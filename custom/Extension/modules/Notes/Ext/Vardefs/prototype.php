8d00f9928c8991cfed70dc93f7c5d08aec11b7c2
Adam VandeMore <avandemore@datasyncorp.com>
2012-04-26 00:10:27 -0500

    PROTRACK-9 Note's module HTML field/Rich Text Editor

---
+++ custom/Extension/modules/Notes/Ext/Vardefs/prototype.php
@@ -0,0 +1,5 @@
+<?php
+
+//$dictionary["Note"]["fields"]["description"]["type"] = 'html';
+
+?>
---
+++ custom/Extension/modules/Notes/Ext/Vardefs/sugarfield_description.php
@@ -0,0 +1,5 @@
+<?php
+ // created: 2012-04-25 23:49:08
+$dictionary['Note']['fields']['description']['calculated']=false;
+
+ ?>
