dae9e7716eb8eb223bd1507c64744da44531578c
Uneebe <uneebe@datasyncorp.com>
2012-05-31 18:14:48 +0500

    https://datasync.atlassian.net/browse/PROTRACK-50

---
+++ custom/Extension/modules/Accounts/Ext/Layoutdefs/accounts_contacts_Accounts.php
@@ -0,0 +1,22 @@
+<?php
+$layout_defs["Accounts"]["subpanel_setup"]['accounts_contacts'] = array (
+  'order' => 101,
+  'module' => 'Contacts',
+  'subpanel_name' => 'default',
+  'sort_order' => 'asc',
+  'sort_by' => 'id',
+  'title_key' => 'LBL_CONTACTS_SUBPANEL_TITLE',
+  'get_subpanel_data' => 'accounts_contacts',
+  'top_buttons' => 
+  array (
+    0 => 
+    array (
+      'widget_class' => 'SubPanelTopButtonQuickCreate',
+    ),
+    1 => 
+    array (
+      'widget_class' => 'SubPanelTopSelectButton',
+      'mode' => 'MultiSelect',
+    ),
+  ),
+);
