76538caa7ea8039fe250a2f59d2627b1acb6d2d6
Michael Moser <michaeldmoser@gmail.com>
2012-07-12 10:31:04 -0500

    Update field labels

--- custom/modules/Tasks/language/en_us.lang.php
+++ custom/modules/Tasks/language/en_us.lang.php
@@ -1,11 +1,12 @@
 <?php
-// created: 2012-07-07 12:38:31
+// created: 2012-07-12 10:30:30
 $mod_strings = array (
   'LBL_DD1' => 'Dropdown 1',
   'LBL_DD2' => 'Dropdown 2',
   'LBL_CLOSED_STATUS' => 'Closed Status',
   'LBL_TASKS_TASKS_FROM_TASKS_L_TITLE' => 'Related Tasks',
   'LBL_ASSIGNED_TO_CLIENT' => 'Assigned To Client',
-  'LBL_LIST_RELATED_TO' => 'Parent Task',
+  'LBL_LIST_RELATED_TO' => 'Related Record',
   'LBL_DETAILVIEW_PANEL1' => 'New Panel 1',
+  'LBL_PARENT_TASKS' => 'Waiting for',
 );
