fdd838f4ba329f36e23d69364352f5cb795d9217
Uneebe <uneebe@datasyncorp.com>
2012-08-31 20:32:15 +0500

    https://datasync.atlassian.net/browse/PROTRACK-72

---
+++ custom/Extension/modules/Meetings/Ext/Vardefs/sugarfield_attach_to_group_id.php
@@ -0,0 +1,11 @@
+<?php
+$dictionary["Meeting"]["fields"]["attach_to_group_id"] = array (
+  'name' => 'attach_to_group_id',
+  'vname' => 'LBL_ATTACH_TO_GROUP_ID_MEETINGS',
+  'type' => 'varchar',
+  'len' => '255',
+  'audited'=>false,
+  'required'=>false,
+  'comment' => 'Meeting Attach To Group'
+);
+
