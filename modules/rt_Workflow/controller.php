e2c77223db613e32532606dab166062c36ebccc5
Abdul Akram <abdul>
2012-09-25 17:44:46 +0500

    PROTRACK-97
    Calculate Start/Target date for tasks in Assign Workflow

--- modules/rt_Workflow/controller.php
+++ modules/rt_Workflow/controller.php
@@ -4,15 +4,56 @@ require_once('modules/rt_Workflow/TreeData.php');
 
 class rt_WorkflowController extends SugarController {
 		
-	function getTaskTemplateIds($tasks_templates, &$ids){
+	function setTaskTemplateIds($tasks_templates, &$ids){
 		foreach($tasks_templates as $tasks_template){
 			$ids[] = $tasks_template['id'];
 			
 			//Calling recursively for childrens
 			if(isset($tasks_template['children']) && !empty($tasks_template['children'])){
-				$this->getTaskTemplateIds($tasks_template['children'], $ids);
+				$this->setTaskTemplateIds($tasks_template['children'], $ids);
 			}
 		}
+	}
+	
+	function setTaskDates($tasks_templates, &$dates, &$daysOut, $reversed, $startStamp, $top = false){
+		$minStamp = $startStamp;
+		
+		foreach($tasks_templates as $tasks_template){
+			if(!isset($daysOut[$tasks_template['id']]) || empty($daysOut[$tasks_template['id']])){
+				$daysOut[$tasks_template['id']] = 0;
+			}
+			
+			$tmpStamp = $startStamp;
+			
+			if(!$reversed){
+				if(!$top){
+					$tmpStamp = strtotime("+" . $daysOut[$tasks_template['id']] . " day", $tmpStamp);
+				}
+			}
+			
+			//Calling recursively for childrens
+			if(isset($tasks_template['children']) && !empty($tasks_template['children'])){
+				$tmpStampRev = $this->setTaskDates($tasks_template['children'], $dates, $daysOut, $reversed, $tmpStamp);
+				
+				if($reversed){
+					$tmpStamp = strtotime("-" . $daysOut[$tasks_template['id']] . " day", $tmpStampRev);
+				}
+			}
+			
+			//if start date is less then today
+			if($tmpStamp < strtotime("now")){
+				$tmpStamp = strtotime("now");
+			}
+			
+			$dates[$tasks_template['id']] = date('Y-m-d', $tmpStamp);
+			
+			//To send minimum timestamp to parent
+			if($tmpStamp < $minStamp){
+				$minStamp = $tmpStamp;
+			}
+		}
+			
+		return $minStamp;
 	}
 	
 	function action_assign(){
@@ -20,10 +61,12 @@ class rt_WorkflowController extends SugarController {
 			
 		if(isset($_REQUEST['parent_id']) && !empty($_REQUEST['parent_id'])){
 			$ids = array();
+			$dates = array();
+			$daysOut = array();
 			
 			$tasks_templates = TreeData::getData($this->bean->id);
 			if(isset($tasks_templates['children']) && !empty($tasks_templates['children'])){
-				$this->getTaskTemplateIds($tasks_templates['children'], $ids);
+				$this->setTaskTemplateIds($tasks_templates['children'], $ids);
 			}
 			
 			$sql = "SELECT ".
@@ -42,7 +85,23 @@ class rt_WorkflowController extends SugarController {
 					$array_new_ids[$row['id']] = create_guid();
 				}
 				
+				if(!isset($daysOut[$row['id']])){
+					$daysOut[$row['id']] = intval($row['days_out']);
+				}
+				
 				$taskTemplateRecords[] = $row;
+			}
+			
+			if(isset($tasks_templates['children']) && !empty($tasks_templates['children'])){
+				$reversed = (isset($_REQUEST['workflow_counts_down_to_target_date']) && !empty($_REQUEST['workflow_counts_down_to_target_date']));
+				$startStamp = (isset($_REQUEST['start_date']) && !empty($_REQUEST['start_date'])) ? strtotime($_REQUEST['start_date']) : strtotime("now");
+				
+				//if start date is less then today
+				if($startStamp < strtotime("now")){
+					$startStamp = strtotime("now");
+				}
+				
+				$this->setTaskDates($tasks_templates['children'], $dates, $daysOut, $reversed, $startStamp, true);
 			}
 			
 			foreach($taskTemplateRecords as $record){
@@ -53,19 +112,26 @@ class rt_WorkflowController extends SugarController {
 				}
 								
 				//Making SQL statement
-				$record['id'] = $array_new_ids[$record['id']];
-				if(!empty($record['parent_tasks_id'])){
-					$record['parent_tasks_id'] = $array_new_ids[$record['parent_tasks_id']];
-				}
-				
 				$record['parent_type'] = $_REQUEST['parent_type'];
 				$record['parent_id'] = $_REQUEST['parent_id'];
 				if(isset($record['days_out'])){
 					unset($record['days_out']);
 				}
 				
+				//Attaching to project from request
 				if(isset($_REQUEST['add_to_project_id']) && !empty($_REQUEST['add_to_project_id'])){
 					$record['project_id'] = $_REQUEST['add_to_project_id'];
+				}
+				
+				//Adding calculated start date
+				if(isset($dates[$record['id']]) && !empty($dates[$record['id']])){
+					$record['date_start'] = $dates[$record['id']];
+				}
+				
+				//Adjusting ids
+				$record['id'] = $array_new_ids[$record['id']];
+				if(!empty($record['parent_tasks_id'])){
+					$record['parent_tasks_id'] = $array_new_ids[$record['parent_tasks_id']];
 				}
 				
 				$keys = implode(',' , array_keys($record));
