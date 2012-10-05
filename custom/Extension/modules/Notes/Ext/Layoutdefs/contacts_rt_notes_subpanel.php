d5a13b6495fb526b107e4552dbee81290a5e3a8c
Uneebe <uneebe@datasyncorp.com>
2012-10-05 16:57:11 +0500

    PROTRACK-100

--- custom/Extension/modules/Notes/Ext/Layoutdefs/contacts_rt_notes_subpanel.php
+++ custom/Extension/modules/Notes/Ext/Layoutdefs/contacts_rt_notes_subpanel.php
@@ -2,10 +2,10 @@
 $layout_defs["Notes"]["subpanel_setup"]['contacts_rt_notes'] = array (
    'order' => 100,
    'module' => 'Contacts',
-      'subpanel_name' => 'default',
+   'subpanel_name' => 'default',
    'sort_order' => 'asc',
       'sort_by' => 'id', 
-	    'title_key' => 'LBL_RT_NOTES_CONTACTS',  
+	    'title_key' => 'LBL_NOTES_RT_CONTACTS_TITLE',  
 		 'get_subpanel_data' => 'contacts_notes', 
 		   'top_buttons' =>    array (    
 			    0 =>      array (       'widget_class' => 'SubPanelTopButtonQuickCreate',     ),  
