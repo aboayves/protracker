1bb3ad4ef3dc8c0660f00ad6d23f77b1254c3d78
Abdul Akram <abdul>
2012-10-11 13:07:17 +0500

    PROTRACK-136
    Getting wrong right-click menu in Task Tree View *Sometimes*

--- custom/modules/Tasks/tree.js
+++ custom/modules/Tasks/tree.js
@@ -1,90 +1,61 @@
-YAHOO.util.Event.onDOMReady(generateTree);
+YAHOO.util.Event.onDOMReady(loadTree);
 var disable_ajax = 1;
 var oCurrentTextNode = null;
 var tree;
 
+var oContextMenu = new YAHOO.widget.ContextMenu( 
+	"mytreecontextmenu", 
+	{ 
+		trigger: "tree_panel3", 
+		lazyload: true, 
+		itemdata: [ 
+		
+			{ text: "Open", onclick: { fn: openNode } } ,
+			{ text: "Edit", onclick: { fn: editNode } } ,
+			{ text: "Delete", onclick: { fn: deleteNode } } 
+		] 
+	} 
+); 
+oContextMenu.subscribe("triggerContextMenu", onTriggerContextMenu);
+
 function generateTree(){
-	if(disable_ajax)
-	{
-		tree = new YAHOO.widget.TreeView("tree_panel3", tree_data1); 
-		tree.render();
-			var oContextMenu = new YAHOO.widget.ContextMenu( 
-			"mytreecontextmenu", 
-			{ 
-				trigger: "tree_panel3", 
-				lazyload: true, 
-				itemdata: [ 
-				
-					{ text: "Open", onclick: { fn: openNode } } ,
-					{ text: "Edit", onclick: { fn: editNode } } ,
-					{ text: "Delete", onclick: { fn: deleteNode } } 
-				] 
-			} 
-		); 
-		oContextMenu.subscribe("triggerContextMenu", onTriggerContextMenu);
-	}
-	else
-	{
-		var taskID = document.getElementsByName('record')[0].value;
-		var taskName = document.getElementsByName('task_name')[0].value;
-		loadNodeData(taskID, taskName);
+	var taskID = document.getElementsByName('record')[0].value;
+	var taskName = document.getElementsByName('task_name')[0].value;
+	loadNodeData(taskID, taskName);
+}
+
+function loadTree(){
+	if(disable_ajax){
+		make_tree(tree_data1);
+	}else{
+		generateTree();
 	}
 	
 	disable_ajax = 0;
-	setTimeout("generateTree();", 10000);
+	setTimeout("loadTree();", 10000);
 }
 
- function onTriggerContextMenu(p_oEvent) 
- {
-
-          var oTarget = this.contextEventTarget; 
+ function onTriggerContextMenu(p_oEvent){
+	var oTarget = this.contextEventTarget; 
 	
-	    oCurrentTextNode = tree.getNodeByElement(oTarget); 	 
-	    if (!oCurrentTextNode) { 
-	
-	 //    alert("no current text node !");
-		    this.cancel(); 
-	 
-	    } 
+	oCurrentTextNode = tree.getNodeByElement(oTarget); 	 
+	if (!oCurrentTextNode) {
+		this.cancel();
+	} 
 }
 
- function openNode()
- {
-	
-		window.open(oCurrentTextNode.getLabelEl().getAttribute("href"));
-		 
-		 
-	 	
+ function openNode(){
+	window.open("index.php?module=Tasks&action=DetailView&record=" + oCurrentTextNode.data.id);
  }
-  function editNode()
- {
-		
-		
-		window.open((oCurrentTextNode.getLabelEl().getAttribute("href")).replace(/DetailView/g, "EditView"));
-	
-		 
-		 
-	 	
+
+ function editNode(){
+		window.open("index.php?module=Tasks&action=EditView&record=" + oCurrentTextNode.data.id);
  }
-  function deleteNode()
- { 
- 
-var id= (oCurrentTextNode.getLabelEl().getAttribute("href")).split("record=");
-			id=id[1].substr(0,36);
 
- confirmDelete(id);
-
-
- 
-  }
-
- 
-	
-		 
-		 
-	 	
-
-
+ function deleteNode(){ 
+	confirmDelete(oCurrentTextNode.data.id); 
+}
+  
 function loadNodeData(taskID, taskName)  {
 	var pendingOnly = 0;
 	var moreThen90 = 0;
@@ -104,7 +75,7 @@ function loadNodeData(taskID, taskName)  {
 
         success: function(oResponse) {
             YAHOO.log("XHR transaction was successful.", "info", "example");			
-            success_tree(eval(YAHOO.lang.JSON.parse(oResponse.responseText)));
+            make_tree(eval(YAHOO.lang.JSON.parse(oResponse.responseText)));
         },
         failure: function(oResponse) {
             YAHOO.log("Failed to process XHR transaction.", "info", "example");
@@ -116,7 +87,7 @@ function loadNodeData(taskID, taskName)  {
 }
 
 
-function success_tree(data1){
-   var tree = new YAHOO.widget.TreeView("tree_panel3", data1); 
-    tree.render();
+function make_tree(data1){
+   tree = new YAHOO.widget.TreeView("tree_panel3", data1); 
+   tree.render();
 }
