9e8f159d21ab1a66dd07e3440bc6ac144b122c77
Uneebe <uneebe@datasyncorp.com>
2012-10-15 19:42:12 +0500

    PROTRACK-153

--- custom/modules/av_Groups/PurgeExpiredMembers.php
+++ custom/modules/av_Groups/PurgeExpiredMembers.php
@@ -1,25 +1,34 @@
 <?php
 global $db, $timedate;
-
-$query="SELECT av_groups.membership_expiration_days,av_group_membership.date_entered,av_group_membership.id
-	  FROM av_group_membership
-	  LEFT JOIN av_groups ON av_groups.deleted=0 AND av_groups.id = av_group_membership.av_groups_id
-	  WHERE av_group_membership.deleted=0 AND av_group_membership.av_groups_id='{$_REQUEST['record']}' AND av_group_membership.include=1";
+$current_date=$timedate->nowDbDate();
+$query="
+		SELECT av_group_membership.expiration_date, av_group_membership.id
+		FROM av_group_membership
+		LEFT JOIN av_groups ON ( av_groups.deleted=0 AND av_groups.id = av_group_membership.av_groups_id )
+		WHERE 
+			av_group_membership.deleted=0 
+			AND 
+			av_group_membership.av_groups_id='{$this->bean->id}' 
+			AND 
+			av_group_membership.include=1 
+			AND 
+			(
+				av_group_membership.expiration_date IS NOT NULL 
+				OR 
+				av_group_membership.expiration_date !=''
+			)
+			AND 
+			av_group_membership.expiration_date < '{$current_date}'
+		";
 
 $members = $db->query($query);
-while ($member = $db->fetchByAssoc($members))
-{	
-	list($m, $d, $y) = explode("/", $timedate->nowDate());
-	list($y1, $m1, $d1) = explode("-", $timedate->getDatePart($member['date_entered']));
-	$daysstart =(strtotime($y.'-'.$m.'-'.$d) - strtotime($y1.'-'.$m1.'-'.$d1)) / (60 * 60 * 24);
-
-	if($daysstart>$member['membership_expiration_days'])
-	{
-		$querry="UPDATE `av_group_membership` SET `deleted`='1' where id='{$member['id']}'";
+while ($member = $db->fetchByAssoc($members)) {	
+	if($current_date>$member['expiration_date'] && !empty($member['expiration_date'])) {
+		$querry="UPDATE `av_group_membership` SET `deleted`='2' where id='{$member['id']}'";
 		$db->query($querry);
 	}
 }
 
-SugarApplication::redirect("index.php?module=av_Groups&action=DetailView&record={$_REQUEST['record']}");
+SugarApplication::redirect("index.php?module=av_Groups&action=DetailView&record={$this->bean->id}");
 
 ?>
