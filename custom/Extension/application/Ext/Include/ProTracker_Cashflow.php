d76167e6ca6de921002ab96e6c7c222fafbdfc20
Ron Walters <ron.walters@protracker.com>
2012-10-14 21:43:57 -0400

    cashflow module added, incomplete

---
+++ custom/Extension/application/Ext/Include/ProTracker_Cashflow.php
@@ -0,0 +1,7 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['av_Cashflow'] = 'av_Cashflow';
+$beanFiles['av_Cashflow'] = 'modules/av_Cashflow/av_Cashflow.php';
+$moduleList[] = 'av_Cashflow';
+
+?>
---
+++ custom/modules/av_Cashflow/metadata/editviewdefs.php
@@ -0,0 +1,63 @@
+<?php
+$module_name = 'av_Cashflow';
+$viewdefs [$module_name] = 
+array (
+  'EditView' => 
+  array (
+    'templateMeta' => 
+    array (
+      'maxColumns' => '2',
+      'widths' => 
+      array (
+        0 => 
+        array (
+          'label' => '10',
+          'field' => '30',
+        ),
+        1 => 
+        array (
+          'label' => '10',
+          'field' => '30',
+        ),
+      ),
+      'useTabs' => false,
+      'syncDetailEditViews' => true,
+    ),
+    'panels' => 
+    array (
+      'default' => 
+      array (
+        0 => 
+        array (
+          0 => 'name',
+        ),
+        1 => 
+        array (
+          0 => 'description',
+        ),
+        2 => 
+        array (
+          0 => 'assigned_user_name',
+          1 => 
+          array (
+            'name' => 'year',
+            'comment' => '',
+            'label' => 'LBL_YEAR',
+          ),
+        ),
+        3 => 
+        array (
+          0 => 
+          array (
+            'name' => 'team_name',
+            'displayParams' => 
+            array (
+              'display' => true,
+            ),
+          ),
+        ),
+      ),
+    ),
+  ),
+);
+?>
