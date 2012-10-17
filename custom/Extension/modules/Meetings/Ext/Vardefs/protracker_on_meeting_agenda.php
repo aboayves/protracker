04773409a57fcb93bef62ed37e2104b598acfd35
Ron Walters <ron.walters@protracker.com>
2012-10-17 16:43:06 -0400

    campaigns module update

---
+++ custom/Extension/modules/Meetings/Ext/Vardefs/protracker_on_meeting_agenda.php
@@ -0,0 +1,18 @@
+<?php
+$dictionary["Meeting"]["fields"]["on_meeting_agenda"] = array (
+	'name' => 'on_meeting_agenda',
+	'vname' => 'LBL_ON_MEETING_AGENDA',
+	'type' => 'bool',
+	'audited' => '0',
+	'required' => '0',
+	'comment' => '',
+	'default' => '',
+	'massupdate' => '1',
+	'reportable' => '1',
+	'importable' => '1',
+	'duplicate_merge' => 'Enabled',
+	'duplicate_merge_dom_value' => '1',
+	'help' => '',
+	'group' => '',
+);
+?>
