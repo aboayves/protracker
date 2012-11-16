e2c37e90b89979c432ccc45791d19cc7b54b2359
Uneebe <uneebe@datasyncorp.com>
2012-11-16 18:51:25 +0500

    PROTRACK-128

---
+++ custom/Extension/modules/Administration/Ext/Administration/admin.php
@@ -0,0 +1,26 @@
+<?php
+$admin_option_defs = array();
+$admin_option_defs['UnifiedLogin'] = array(
+'themes/default/images/website_links', 'LBL_UNIFIED_LOGIN', 'LBL_REG_UNIFIED_LOGIN', 'javascript:dialogForURL();');
+// Loop through the menus and add to the Users group
+foreach ($admin_group_header as $key => $values)
+{
+	if ($values[0] == 'LBL_ADMINISTRATION_HOME_TITLE')
+	{
+		$admin_group_header[$key][3]['Administration']['UnifiedLogin'] = $admin_option_defs['UnifiedLogin'];
+	}
+}
+echo '<div id="dialogForURL" title="Enter REST URL" style="display:none"><br/><br/>
+		  <form name="input" action="index.php?module=Users&action=register_admin" method="POST">
+			  Enter URL: <input type="text" id="rest_url" name="rest_url"/> &nbsp;
+			  <input type="submit" id="create_admin" name="create_admin" value="Go"/>
+		  </form>
+	 </div>';
+echo '<script type="text/javascript">
+		function dialogForURL()
+		{
+			 $( "#dialogForURL" ).dialog({ width: 400 });
+		}
+		
+      </script>';
+?>
---
+++ custom/Extension/modules/Administration/Ext/Language/en_us.custom_link.php
@@ -0,0 +1,3 @@
+<?php
+$mod_strings['LBL_UNIFIED_LOGIN'] = 'Unified Login';
+$mod_strings['LBL_REG_UNIFIED_LOGIN'] = 'Register';
