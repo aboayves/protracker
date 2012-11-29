b45b9635ee6dafa996b7fbf0275f997ca1634f79
Uneebe <uneebe@datasyncorp.com>
2012-11-29 20:18:11 +0500

    PROTRACK-216

---
+++ custom/modules/Reports/logic_hooks.php
@@ -0,0 +1,4 @@
+<?php
+$hook_array['after_save'] = Array();
+$hook_array['after_save'][] = Array(1, 'return_to_groups', 'custom/modules/Reports/return_to_groups.php', 'return_to_groups', 'return_to_groups_popup');
+?>
