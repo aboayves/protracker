d96fe3403110f6d054b3617873321ed215be0a87
Abdul Akram <abdul>
2012-12-04 15:03:14 +0500

    PROTRACK-262 Create module group filters according the this spreadsheet

--- custom/include/tabConfig.php
+++ custom/include/tabConfig.php
@@ -1,56 +1,98 @@
 <?php
-// created: 2012-11-19 02:51:00
+// created: 2012-12-04 10:01:59
 $GLOBALS['tabStructure'] = array (
-  'LBL_TABGROUP_ACTIVITIES' => 
-  array (
-    'label' => 'LBL_TABGROUP_ACTIVITIES',
-    'modules' => 
-    array (
-      0 => 'Home',
-      1 => 'Calendar',
-      2 => 'Calls',
-      3 => 'Meetings',
-      4 => 'Emails',
-      5 => 'Tasks',
-      6 => 'Notes',
-    ),
-  ),
-  'LBL_TABGROUP_MARKETING' => 
-  array (
-    'label' => 'LBL_TABGROUP_MARKETING',
-    'modules' => 
-    array (
-      0 => 'Home',
-      1 => 'Accounts',
-      2 => 'Contacts',
-      3 => 'Leads',
-      4 => 'Campaigns',
-      5 => 'Prospects',
-      6 => 'ProspectLists',
-    ),
-  ),
-  'LBL_TABGROUP_SALES' => 
-  array (
-    'label' => 'LBL_TABGROUP_SALES',
-    'modules' => 
-    array (
-      0 => 'Home',
-      1 => 'Accounts',
-      2 => 'Contacts',
-      3 => 'Opportunities',
-      4 => 'Leads',
-      5 => 'Contracts',
-      6 => 'Quotes',
-      7 => 'Products',
-      8 => 'Forecasts',
-    ),
-  ),
   'LBL_GROUPTAB6_1353291972' => 
   array (
     'label' => 'LBL_GROUPTAB6_1353291972',
     'modules' => 
     array (
       0 => 'av_Accounts',
+      1 => 'av_Account_Histories',
+      2 => 'av_Account_Categories',
+      3 => 'av_Beneficiaries',
+      4 => 'av_Net_Worth',
+      5 => 'av_Positions',
+      6 => 'av_Transaction_Schedule',
+      7 => 'av_Transactions',
+      8 => 'av_Trustees',
+    ),
+  ),
+  'LBL_TABGROUP_ACTIVITIES' => 
+  array (
+    'label' => 'LBL_TABGROUP_ACTIVITIES',
+    'modules' => 
+    array (
+      0 => 'Meetings',
+      1 => 'Notes',
+      2 => 'Tasks',
+      3 => 'Calls',
+      4 => 'Emails',
+      5 => 'av_Workflow',
+      6 => 'av_Task_Template',
+    ),
+  ),
+  'LBL_GROUPTAB2_1354614062' => 
+  array (
+    'label' => 'LBL_GROUPTAB2_1354614062',
+    'modules' => 
+    array (
+      0 => 'Contracts',
+      1 => 'av_Payments',
+      2 => 'Quotes',
+    ),
+  ),
+  'LBL_GROUPTAB3_1354614142' => 
+  array (
+    'label' => 'LBL_GROUPTAB3_1354614142',
+    'modules' => 
+    array (
+      0 => 'av_IPS',
+      1 => 'av_Office_Gifts',
+      2 => 'av_Suitability',
+    ),
+  ),
+  'LBL_GROUPTAB4_1354614267' => 
+  array (
+    'label' => 'LBL_GROUPTAB4_1354614267',
+    'modules' => 
+    array (
+      0 => 'Documents',
+    ),
+  ),
+  'LBL_GROUPTAB5_1354614342' => 
+  array (
+    'label' => 'LBL_GROUPTAB5_1354614342',
+    'modules' => 
+    array (
+      0 => 'av_Estate_Admin',
+      1 => 'av_Estate_Documents',
+      2 => 'av_Final_Arrangements',
+      3 => 'av_Gift_Tax_Credits',
+      4 => 'av_Gifting',
+      5 => 'av_IRD',
+      6 => 'av_Trusts',
+      7 => 'av_Wills',
+    ),
+  ),
+  'LBL_GROUPTAB6_1354614495' => 
+  array (
+    'label' => 'LBL_GROUPTAB6_1354614495',
+    'modules' => 
+    array (
+      0 => 'av_Offices',
+      1 => 'av_Reviews',
+      2 => 'av_Scoring',
+      3 => 'av_Service_Team',
+      4 => 'av_Commissions',
+    ),
+  ),
+  'LBL_GROUPTAB7_1354614605' => 
+  array (
+    'label' => 'LBL_GROUPTAB7_1354614605',
+    'modules' => 
+    array (
+      0 => 'av_Cashflow',
+      1 => 'av_Goals',
     ),
   ),
   'LBL_GROUPTAB7_1353291973' => 
@@ -68,15 +110,54 @@ $GLOBALS['tabStructure'] = array (
       7 => 'av_Vehicle_Insurance',
     ),
   ),
-  'LBL_GROUPTAB8_1353291973' => 
+  'LBL_GROUPTAB9_1354614720' => 
   array (
-    'label' => 'LBL_GROUPTAB8_1353291973',
+    'label' => 'LBL_GROUPTAB9_1354614720',
     'modules' => 
     array (
-      0 => 'av_Trusts',
-      1 => 'av_Wills',
-      2 => 'av_Final_Arrangements',
-      3 => 'av_Estate_Documents',
+      0 => 'Project',
+    ),
+  ),
+  'LBL_GROUPTAB10_1354614768' => 
+  array (
+    'label' => 'LBL_GROUPTAB10_1354614768',
+    'modules' => 
+    array (
+      0 => 'av_Professionals',
+      1 => 'av_Relatives',
+    ),
+  ),
+  'LBL_GROUPTAB11_1354614824' => 
+  array (
+    'label' => 'LBL_GROUPTAB11_1354614824',
+    'modules' => 
+    array (
+      0 => 'av_Medicare',
+      1 => 'av_Pensions',
+      2 => 'av_Retirement_Projections',
+      3 => 'av_Social_Security',
+    ),
+  ),
+  'LBL_TABGROUP_SALES' => 
+  array (
+    'label' => 'LBL_TABGROUP_SALES',
+    'modules' => 
+    array (
+      0 => 'Campaigns',
+      1 => 'Opportunities',
+      2 => 'Leads',
+      3 => 'Prospects',
+      4 => 'ProspectLists',
+    ),
+  ),
+  'LBL_GROUPTAB13_1354615174' => 
+  array (
+    'label' => 'LBL_GROUPTAB13_1354615174',
+    'modules' => 
+    array (
+      0 => 'av_Keywords',
+      1 => 'av_Account_Types',
+      2 => 'av_Activity_Types',
     ),
   ),
   'LBL_GROUPTAB9_1353291973' => 
@@ -84,43 +165,9 @@ $GLOBALS['tabStructure'] = array (
     'label' => 'LBL_GROUPTAB9_1353291973',
     'modules' => 
     array (
-      0 => 'av_Tax_Returns',
-      1 => 'av_Tax_Estimates',
-    ),
-  ),
-  'LBL_TABGROUP_COLLABORATION' => 
-  array (
-    'label' => 'LBL_TABGROUP_COLLABORATION',
-    'modules' => 
-    array (
-      0 => 'Home',
-      1 => 'Emails',
-      2 => 'Documents',
-      3 => 'Project',
-      4 => 'KBDocuments',
-    ),
-  ),
-  'LBL_TABGROUP_REPORTS' => 
-  array (
-    'label' => 'LBL_TABGROUP_REPORTS',
-    'modules' => 
-    array (
-      0 => 'Home',
-      1 => 'Reports',
-      2 => 'Forecasts',
-    ),
-  ),
-  'LBL_TABGROUP_SUPPORT' => 
-  array (
-    'label' => 'LBL_TABGROUP_SUPPORT',
-    'modules' => 
-    array (
-      0 => 'Home',
-      1 => 'Accounts',
-      2 => 'Contacts',
-      3 => 'Cases',
-      4 => 'Bugs',
-      5 => 'KBDocuments',
+      0 => 'av_Tax_Estimates',
+      1 => 'av_Tax_Returns',
+      2 => 'av_Tax_Forms',
     ),
   ),
 );
