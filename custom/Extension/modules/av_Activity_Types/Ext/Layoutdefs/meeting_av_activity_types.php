8ca62b8f8c0373aeef336a913d9fa1610a523da6
Ron Walters <ron.walters@protracker.com>
2012-12-17 15:11:47 -0500

    Meetings
    added relationship to activity types

---
+++ custom/Extension/modules/av_Activity_Types/Ext/Layoutdefs/meeting_av_activity_types.php
@@ -0,0 +1,22 @@
+<?php
+// created: 2012-12-17 15:01:03
+$layout_defs["av_Activity_Types"]["subpanel_setup"]["meeting_av_activity_types"] = array (
+	'order' => 100,
+	'module' => 'Meeting',
+	'subpanel_name' => 'default',
+	'title_key' => 'LBL_MEETING',
+	'get_subpanel_data' => 'meeting_av_activity_types',
+	'top_buttons' => 
+	array (
+		0 => 
+		array (
+			'widget_class' => 'SubPanelTopButtonQuickCreate',
+		),
+		1 => 
+		array (
+			'widget_class' => 'SubPanelTopSelectButton',
+			'mode' => 'MultiSelect',
+		),
+	),
+);
+
---
+++ custom/Extension/modules/av_Activity_Types/Ext/Vardefs/protracker_Meeting.php
@@ -0,0 +1,11 @@
+<?php
+// created: 2012-12-17 15:01:03
+$dictionary["av_Activity_Types"]["fields"]["meeting_av_activity_types"] = array (
+	'name' => 'meeting_av_activity_types',
+	'type' => 'link',
+	'relationship' => 'meeting_av_activity_types',
+	'source' => 'non-db',
+	'side' => 'right',
+	'vname' => 'LBL_MEETING',
+);
+?>
