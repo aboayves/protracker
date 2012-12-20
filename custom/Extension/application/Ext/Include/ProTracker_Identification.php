5bb381f289e7611d9db1db2c9abd6e8630cc634b
Ron Walters <ron.walters@protracker.com>
2012-12-19 22:55:38 -0500

    Dropped Estate Admin and Identification Modules
    Estate admin fields absorbed into contacts
    Identification fields absorbed into contacts

--- custom/Extension/application/Ext/Include/ProTracker_Identification.php
+++ custom/Extension/application/Ext/Include/ProTracker_Identification.php
@@ -1,9 +1,3 @@
 <?php 
- //WARNING: The contents of this file are auto-generated
-$beanList['av_Identification'] = 'av_Identification';
-$beanFiles['av_Identification'] = 'modules/av_Identification/av_Identification.php';
-$modules_exempt_from_availability_check['av_Identification'] = 'av_Identification';
-$report_include_modules['av_Identification'] = 'av_Identification';
-$modInvisList[] = 'av_Identification';
 
 ?>
--- custom/Extension/modules/Accounts/Ext/WirelessLayoutdefs/accounts_av_identification_1_Accounts.php
+++
@@ -1,9 +0,0 @@
-<?php
- // created: 2012-10-14 16:31:53
-$layout_defs["Accounts"]["subpanel_setup"]['accounts_av_identification_1'] = array (
-  'order' => 100,
-  'module' => 'av_Identification',
-  'subpanel_name' => 'default',
-  'title_key' => 'LBL_ACCOUNTS_AV_IDENTIFICATION_1_FROM_AV_IDENTIFICATION_TITLE',
-  'get_subpanel_data' => 'accounts_av_identification_1',
-);
