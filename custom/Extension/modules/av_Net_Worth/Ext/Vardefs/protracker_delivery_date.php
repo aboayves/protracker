075c128739d21e6f9309dfd109d1621502c9e05c
Ron Walters <ron.walters@protracker.com>
2012-12-19 20:59:03 -0500

    Net Worth
    updated relationship - todo fix Stampit()

--- custom/Extension/modules/av_Net_Worth/Ext/Vardefs/protracker_delivery_date.php
+++ custom/Extension/modules/av_Net_Worth/Ext/Vardefs/protracker_delivery_date.php
@@ -1,17 +1,18 @@
 <?php
+// created: 2012-12-19 20:39:48
 $dictionary["av_Net_Worth"]["fields"]["delivery_date"] = array (
 	'name' => 'delivery_date',
 	'vname' => 'LBL_DELIVERY_DATE',
 	'type' => 'date',
-	'audited' => '0',
-	'required' => '0',
+	'audited' => 0,
+	'required' => 0,
 	'comment' => '',
 	'default' => '',
-	'massupdate' => '1',
-	'reportable' => '1',
-	'importable' => '1',
+	'massupdate' => True,
+	'reportable' => True,
+	'importable' => True,
 	'duplicate_merge' => 'Enabled',
-	'duplicate_merge_dom_value' => '1',
+	'duplicate_merge_dom_value' => True,
 	'help' => '',
 	'group' => '',
 );
--- custom/Extension/modules/av_Net_Worth/Ext/Vardefs/protracker_net_worth_date.php
+++ custom/Extension/modules/av_Net_Worth/Ext/Vardefs/protracker_net_worth_date.php
@@ -1,17 +1,18 @@
 <?php
+// created: 2012-12-19 20:39:48
 $dictionary["av_Net_Worth"]["fields"]["net_worth_date"] = array (
 	'name' => 'net_worth_date',
 	'vname' => 'LBL_NET_WORTH_DATE',
 	'type' => 'date',
-	'audited' => '0',
-	'required' => '0',
+	'audited' => 0,
+	'required' => 0,
 	'comment' => '',
 	'default' => '',
-	'massupdate' => '1',
-	'reportable' => '1',
-	'importable' => '1',
+	'massupdate' => True,
+	'reportable' => True,
+	'importable' => True,
 	'duplicate_merge' => 'Enabled',
-	'duplicate_merge_dom_value' => '1',
+	'duplicate_merge_dom_value' => True,
 	'help' => '',
 	'group' => '',
 );
