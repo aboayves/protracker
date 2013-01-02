a58824fe6728c8a641828882186b00ca041cdc03
Abdul Akram <abdur.rehman@rolustech.com>
2013-01-02 18:09:42 +0500

    PROTRACK-328 Theme: Bottom bar floats in center of screen on Users detailview.

--- config_override.php
+++ config_override.php
@@ -1,7 +1,7 @@
 <?php
 /***CONFIGURATOR***/
 $sugar_config['disableAjaxUI'] = true;
-$sugar_config['default_module_favicon'] = true;
+$sugar_config['default_module_favicon'] = false;
 $sugar_config['list_max_entries_per_subpanel'] = '20';
 $sugar_config['default_theme'] = 'ProTracker';
 $sugar_config['disabled_themes'] = '';
@@ -14,5 +14,5 @@ $sugar_config['full_text_engine']['']['host'] = '';
 $sugar_config['full_text_engine']['']['port'] = '';
 $sugar_config['full_text_engine']['']['valid'] = true;
 $sugar_config['disabled_languages'] = 'bg_BG,cs_CZ,et_EE,he_IL,hu_HU,ja_JP,nb_NO,sr_RS,ca_ES,pt_br,zh_CN,tr_TR,nl_NL,sv_SE,ru_RU,pl_PL,ro_RO,pt_PT,lt_LT';
-$sugar_config['http_referer']['list'][] = '192.168.1.59';
+$sugar_config['http_referer']['list']['0'] = '192.168.1.59';
 /***CONFIGURATOR***/
--- modules/Users/tpls/DetailViewHeader.tpl
+++ modules/Users/tpls/DetailViewHeader.tpl
@@ -94,9 +94,7 @@ $(document).ready(function(){
 
 </td>
 <td width="100%">
-<table width="100%" border="0" cellspacing="0" cellpadding="0">
 {$PAGINATION}
-</table>
 </td>
 </tr>
 </table>
