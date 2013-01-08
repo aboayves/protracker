c8a7b002bbc4cf1671f6e9bbb1f71a45ae9842c8
Abdul Akram <abdur.rehman@rolustech.com>
2013-01-08 20:43:34 +0500

    Removing old account_contacts relationships

--- custom/Extension/modules/Accounts/Ext/WirelessLayoutdefs/accounts_contacts_1_Accounts.php
+++
@@ -1,9 +0,0 @@
-<?php
- // created: 2012-06-01 13:23:06
-$layout_defs["Accounts"]["subpanel_setup"]['accounts_contacts_1'] = array (
-  'order' => 100,
-  'module' => 'Contacts',
-  'subpanel_name' => 'default',
-  'title_key' => 'LBL_ACCOUNTS_CONTACTS_1_FROM_CONTACTS_TITLE',
-  'get_subpanel_data' => 'accounts_contacts_1',
-);
