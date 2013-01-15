c5a7b9f16fc90263fa815fa964f1f8851daa5e76
Ron Walters <ron.walters@protracker.com>
2013-01-15 16:40:18 -0500

    Services module: added effective dates of service
    case 36904

---
+++ custom/Extension/modules/av_Services/Ext/Vardefs/sugarfield_date_start.php
@@ -0,0 +1,19 @@
+<?php
+// created: 2013-01-15 16:35:53
+$dictionary["av_Services"]["fields"]["date_start"] = array (
+	'name' => 'date_start',
+	'vname' => 'LBL_DATE_START',
+	'type' => 'date',
+	'audited' => 0,
+	'required' => 0,
+	'comment' => '',
+	'default' => '',
+	'massupdate' => True,
+	'reportable' => True,
+	'importable' => True,
+	'duplicate_merge' => 'Enabled',
+	'duplicate_merge_dom_value' => True,
+	'help' => '',
+	'group' => '',
+);
+?>
