33f232ac821d02e1a02240c189507a9c862e3395
Ron Walters <ron.walters@protracker.com>
2013-01-16 16:00:22 -0500

    Services module: added Hourly Rate and Flat Fee fields

---
+++ custom/Extension/modules/av_Services/Ext/Vardefs/sugarfield_flat_fee.php
@@ -0,0 +1,22 @@
+<?php
+// created: 2013-01-16 15:54:55
+$dictionary["av_Services"]["fields"]["flat_fee"] = array (
+	'name' => 'flat_fee',
+	'vname' => 'LBL_FLAT_FEE',
+	'type' => 'currency',
+	'dbtype' => 'decimal(26,2)',
+	'len' => '26',
+	'size' => '20',
+	'audited' => 0,
+	'required' => 0,
+	'comment' => '',
+	'default' => '',
+	'massupdate' => True,
+	'reportable' => True,
+	'importable' => True,
+	'duplicate_merge' => 'Enabled',
+	'duplicate_merge_dom_value' => True,
+	'help' => '',
+	'group' => '',
+);
+?>
