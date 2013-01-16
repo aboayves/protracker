3f6ba4e6f1a28275c63bdbe2e332fa12f097808f
Abdul Akram <abdur.rehman@rolustech.com>
2013-01-16 15:07:47 +0500

    PROTRACK-357 Disable adding users unless a magic URL was added

--- custom/modules/Users/views/view.edit.php
+++ custom/modules/Users/views/view.edit.php
@@ -6,8 +6,8 @@ require_once('modules/Users/views/view.edit.php');
 
 class CustomUsersViewEdit extends UsersViewEdit {
     function display() {
-        if(empty($this->bean->id) && (!isset($_REQUEST['force_create']) || empty($_REQUEST['force_create']))){
-           echo "You are not authorize to create a new user. Please contact your administrator.";
+		if((empty($this->bean->id) || $this->ev->isDuplicate) && (!isset($_REQUEST['force_create']) || empty($_REQUEST['force_create']))){
+           echo "You are not authorized to perform this action. Please contact your Administrator if you wish to add users.";
         }else{
 			parent::display();
 		}
