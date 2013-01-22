09af55b451924c855cab42709bbbdd1455b1f00e
Ron Walters <ron.walters@protracker.com>
2013-01-22 15:06:46 -0500

    Fact Finder module: renamed module to Questionnaire

---
+++ custom/modules/av_Fact_Finder/metadata/subpanels/Account_subpanel_av_fact_finder_accounts.php
@@ -0,0 +1,31 @@
+<?php
+// created: 2013-01-22 19:51:08
+$subpanel_layout['list_fields'] = array (
+  'name' => 
+  array (
+    'vname' => 'LBL_NAME',
+    'widget_class' => 'SubPanelDetailViewLink',
+    'width' => '45%',
+    'default' => true,
+  ),
+  'date_modified' => 
+  array (
+    'vname' => 'LBL_DATE_MODIFIED',
+    'width' => '45%',
+    'default' => true,
+  ),
+  'edit_button' => 
+  array (
+    'widget_class' => 'SubPanelEditButton',
+    'module' => 'av_Fact_Finder',
+    'width' => '4%',
+    'default' => true,
+  ),
+  'remove_button' => 
+  array (
+    'widget_class' => 'SubPanelRemoveButton',
+    'module' => 'av_Fact_Finder',
+    'width' => '5%',
+    'default' => true,
+  ),
+);
