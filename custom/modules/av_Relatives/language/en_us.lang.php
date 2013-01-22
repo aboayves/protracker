6526e835b9537f1ce56414c8d608b5d7a4ae6e03
Ron Walters <ron.walters@protracker.com>
2013-01-22 14:29:35 -0500

    Relatives module: renamed module to Family

---
+++ custom/modules/av_Relatives/language/en_us.lang.php
@@ -0,0 +1,10 @@
+<?php
+// created: 2013-01-22 19:06:44
+$mod_strings = array (
+  'LNK_NEW_RECORD' => 'Create Family',
+  'LNK_LIST' => 'View Family',
+  'LNK_IMPORT_AV_RELATIVES' => 'Import Family',
+  'LBL_LIST_FORM_TITLE' => 'Family List',
+  'LBL_SEARCH_FORM_TITLE' => 'Search Family',
+  'LBL_HOMEPAGE_TITLE' => 'My Family',
+);
