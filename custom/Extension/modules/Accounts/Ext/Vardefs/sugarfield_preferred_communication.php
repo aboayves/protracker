5efc719e7940ea6eefc891f1aad501de6ea2ef97
Ron Walters <ron.walters@protracker.com>
2013-01-22 16:44:39 -0500

    Clients module: added client preference and relationship type fields

---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_preferred_communication.php
@@ -0,0 +1,21 @@
+<?php
+// created: 2013-01-22 15:43:19
+$dictionary["Account"]["fields"]["preferred_communication"] = array (
+	'name' => 'preferred_communication',
+	'vname' => 'LBL_PREFERRED_COMMUNICATION',
+	'type' => 'enum',
+	'options' => 'preferred_communication_list',
+	'len' => '100',
+	'audited' => 0,
+	'required' => 0,
+	'comment' => '',
+	'default' => '',
+	'massupdate' => True,
+	'reportable' => True,
+	'importable' => True,
+	'duplicate_merge' => 'Enabled',
+	'duplicate_merge_dom_value' => True,
+	'help' => '',
+	'group' => '',
+);
+?>
