3edcda152ac04581ec89e9d910eeddc4abe90f48
Ron Walters <ron.walters@protracker.com>
2013-01-22 22:25:58 -0500

    Insurance modules: added commission fields

---
+++ custom/Extension/modules/av_Business_Insurance/Ext/Vardefs/sugarfield_commission_renewal.php
@@ -0,0 +1,22 @@
+<?php
+// created: 2013-01-22 22:01:29
+$dictionary["av_Business_Insurance"]["fields"]["commission_renewal"] = array (
+	'name' => 'commission_renewal',
+	'vname' => 'LBL_COMMISSION_RENEWAL',
+	'type' => 'currency',
+	'dbtype' => 'decimal(26,2)',
+	'len' => '26',
+	'size' => '20',
+	'audited' => 0,
+	'required' => 0,
+	'comment' => '',
+	'default' => '',
+	'massupdate' => True,
+	'reportable' => True,
+	'importable' => True,
+	'duplicate_merge' => 'Enabled',
+	'duplicate_merge_dom_value' => True,
+	'help' => '',
+	'group' => '',
+);
+?>
---
+++ custom/Extension/modules/av_Health_Insurance/Ext/Vardefs/sugarfield_commission_percent.php
@@ -0,0 +1,21 @@
+<?php
+// created: 2013-01-22 22:01:31
+$dictionary["av_Health_Insurance"]["fields"]["commission_percent"] = array (
+	'name' => 'commission_percent',
+	'vname' => 'LBL_COMMISSION_PERCENT',
+	'type' => 'decimal',
+	'precision' => '2',
+	'len' => '26',
+	'audited' => 0,
+	'required' => 0,
+	'comment' => '',
+	'default' => '',
+	'massupdate' => True,
+	'reportable' => True,
+	'importable' => True,
+	'duplicate_merge' => 'Enabled',
+	'duplicate_merge_dom_value' => True,
+	'help' => '',
+	'group' => '',
+);
+?>
