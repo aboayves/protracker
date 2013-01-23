a89c68d89f22c7aaf1b66ff0ab397c35b2b21bde
Ron Walters <ron.walters@protracker.com>
2013-01-22 22:25:58 -0500

    Insurance modules: added commission fields

---
+++ custom/Extension/modules/av_Business_Insurance/Ext/Vardefs/sugarfield_commission_first_year.php
@@ -0,0 +1,22 @@
+<?php
+// created: 2013-01-22 22:01:29
+$dictionary["av_Business_Insurance"]["fields"]["commission_first_year"] = array (
+	'name' => 'commission_first_year',
+	'vname' => 'LBL_COMMISSION_FIRST_YEAR',
+	'type' => 'currency',
+	'dbtype' => 'decimal(26,2)',
+	'len' => '26',
+	'size' => '20',
+	'audited' => 0,
+	'required' => 0,
+	'comment' => '',
+	'default' => '',
+	'massupdate' => True,
+	'reportable' => True,
+	'importable' => True,
+	'duplicate_merge' => 'Enabled',
+	'duplicate_merge_dom_value' => True,
+	'help' => '',
+	'group' => '',
+);
+?>
