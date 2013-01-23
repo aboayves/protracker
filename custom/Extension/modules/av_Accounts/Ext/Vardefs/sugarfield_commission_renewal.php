f197d56dccd7a14143b5dc0518808ef8bbc921c5
Ron Walters <ron.walters@protracker.com>
2013-01-22 21:30:26 -0500

    Accounts module: added commission fields

---
+++ custom/Extension/modules/av_Accounts/Ext/Vardefs/sugarfield_commission_renewal.php
@@ -0,0 +1,22 @@
+<?php
+// created: 2013-01-22 17:00:39
+$dictionary["av_Accounts"]["fields"]["commission_renewal"] = array (
+	'name' => 'commission_renewal',
+	'vname' => 'LBL_COMMISSION_RENEWAL',
+	'type' => 'currency',
+	'dbtype' => 'decimal(26,2)',
+	'len' => '26',
+	'size' => '20',
+	'audited' => 0,
+	'required' => 0,
+	'comment' => '',
+	'default' => '',
+	'massupdate' => True,
+	'reportable' => True,
+	'importable' => True,
+	'duplicate_merge' => 'Enabled',
+	'duplicate_merge_dom_value' => True,
+	'help' => '',
+	'group' => '',
+);
+?>
