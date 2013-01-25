9da91d67fcf308792a84e8aca2c95e9455e61e6e
Ron Walters <ron.walters@protracker.com>
2013-01-25 14:38:54 -0500

    Cashflow module: layout updates

--- custom/Extension/modules/av_Cashflow/Ext/Vardefs/sugarfield_year.php
+++ custom/Extension/modules/av_Cashflow/Ext/Vardefs/sugarfield_year.php
@@ -1,20 +1,22 @@
 <?php
-// created: 2012-12-19 10:57:59
-$dictionary["av_Cashflow"]["fields"]["year"] = array (
-	'name' => 'year',
-	'vname' => 'LBL_YEAR',
-	'type' => 'int',
-	'len' => '11',
-	'audited' => 0,
-	'required' => 0,
-	'comment' => '',
-	'default' => '',
-	'massupdate' => True,
-	'reportable' => True,
-	'importable' => True,
-	'duplicate_merge' => 'Enabled',
-	'duplicate_merge_dom_value' => True,
-	'help' => '',
-	'group' => '',
-);
-?>
+ // created: 2013-01-25 18:25:15
+$dictionary['av_Cashflow']['fields']['year']['name']='year';
+$dictionary['av_Cashflow']['fields']['year']['vname']='LBL_YEAR';
+$dictionary['av_Cashflow']['fields']['year']['type']='int';
+$dictionary['av_Cashflow']['fields']['year']['len']='11';
+$dictionary['av_Cashflow']['fields']['year']['audited']=0;
+$dictionary['av_Cashflow']['fields']['year']['required']=0;
+$dictionary['av_Cashflow']['fields']['year']['comment']='';
+$dictionary['av_Cashflow']['fields']['year']['default']='';
+$dictionary['av_Cashflow']['fields']['year']['massupdate']=0;
+$dictionary['av_Cashflow']['fields']['year']['reportable']=true;
+$dictionary['av_Cashflow']['fields']['year']['importable']=true;
+$dictionary['av_Cashflow']['fields']['year']['duplicate_merge']='enabled';
+$dictionary['av_Cashflow']['fields']['year']['duplicate_merge_dom_value']=true;
+$dictionary['av_Cashflow']['fields']['year']['help']='';
+$dictionary['av_Cashflow']['fields']['year']['group']='';
+$dictionary['av_Cashflow']['fields']['year']['merge_filter']='disabled';
+$dictionary['av_Cashflow']['fields']['year']['calculated']=false;
+$dictionary['av_Cashflow']['fields']['year']['enable_range_search']=false;
+
+ ?>
