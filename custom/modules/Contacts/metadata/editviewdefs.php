19b1066833cbb2bc54a05a3a46be12f181702ec1
Ron Walters <ron.walters@protracker.com>
2013-01-25 14:38:40 -0500

    Contacts module: dropped comments field from layout

--- custom/modules/Contacts/metadata/editviewdefs.php
+++ custom/modules/Contacts/metadata/editviewdefs.php
@@ -178,9 +178,8 @@ array (
         array (
           0 => 
           array (
-            'name' => 'comments',
-            'studio' => 'visible',
-            'label' => 'LBL_COMMENTS',
+            'name' => 'description',
+            'label' => 'LBL_DESCRIPTION',
           ),
           1 => 'team_name',
         ),
