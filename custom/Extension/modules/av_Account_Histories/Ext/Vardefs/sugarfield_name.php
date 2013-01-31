a6e73f9eccccf87d5baa49b91171068b685c8dd5
Ron Walters <ron.walters@protracker.com>
2013-01-31 15:21:31 -0500

    Accounts module: changed field requirements for import

---
+++ custom/Extension/modules/av_Account_Histories/Ext/Vardefs/sugarfield_name.php
@@ -0,0 +1,8 @@
+<?php
+ // created: 2013-01-29 21:50:14
+$dictionary['av_Account_Histories']['fields']['name']['required']=false;
+$dictionary['av_Account_Histories']['fields']['name']['unified_search']=false;
+$dictionary['av_Account_Histories']['fields']['name']['calculated']=false;
+$dictionary['av_Account_Histories']['fields']['name']['importable']='true';
+
+ ?>
