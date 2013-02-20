9f2953b7dc756dbfadf0a99e254a973200309827
Ron Walters <ron.walters@protracker.com>
2013-02-20 14:13:03 -0500

    Clients module: added mailing address, phone fields for mapping to primary contact

---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_mailing_address_state.php
@@ -0,0 +1,20 @@
+<?php
+// created: 2013-02-20 14:03:19
+$dictionary["Account"]["fields"]["mailing_address_state"] = array (
+	'name' => 'mailing_address_state',
+	'vname' => 'LBL_MAILING_ADDRESS_STATE',
+	'type' => 'varchar',
+	'len' => '255',
+	'audited' => 0,
+	'required' => 0,
+	'comment' => '',
+	'default' => '',
+	'massupdate' => True,
+	'reportable' => True,
+	'importable' => True,
+	'duplicate_merge' => 'Enabled',
+	'duplicate_merge_dom_value' => True,
+	'help' => '',
+	'group' => '',
+);
+?>
---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_mailing_address_street.php
@@ -0,0 +1,22 @@
+<?php
+// created: 2013-02-20 14:03:19
+$dictionary["Account"]["fields"]["mailing_address_street"] = array (
+	'name' => 'mailing_address_street',
+	'vname' => 'LBL_MAILING_ADDRESS_STREET',
+	'type' => 'address',
+	'dbtype' => 'varchar',
+	'options' => '',
+	'len' => '255',
+	'audited' => 0,
+	'required' => 0,
+	'comment' => '',
+	'default' => '',
+	'massupdate' => True,
+	'reportable' => True,
+	'importable' => True,
+	'duplicate_merge' => 'Enabled',
+	'duplicate_merge_dom_value' => True,
+	'help' => '',
+	'group' => '',
+);
+?>
