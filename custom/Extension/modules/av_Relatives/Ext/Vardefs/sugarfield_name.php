95ea612f81787255c481d61331183dd3df9f55c4
Ron Walters <ron.walters@protracker.com>
2013-02-22 22:39:32 -0500

    Relatives module: layout updates

---
+++ custom/Extension/modules/av_Relatives/Ext/Vardefs/sugarfield_name.php
@@ -0,0 +1,8 @@
+<?php
+ // created: 2013-02-23 03:12:28
+$dictionary['av_Relatives']['fields']['name']['required']=false;
+$dictionary['av_Relatives']['fields']['name']['unified_search']=false;
+$dictionary['av_Relatives']['fields']['name']['calculated']=false;
+$dictionary['av_Relatives']['fields']['name']['importable']='true';
+
+ ?>
---
+++ custom/modules/av_Relatives/metadata/listviewdefs.php
@@ -0,0 +1,42 @@
+<?php
+$module_name = 'av_Relatives';
+$listViewDefs [$module_name] = 
+array (
+  'TYPE' => 
+  array (
+    'type' => 'varchar',
+    'default' => true,
+    'label' => 'LBL_TYPE',
+    'width' => '10%',
+  ),
+  'DESCRIPTION' => 
+  array (
+    'type' => 'text',
+    'label' => 'LBL_DESCRIPTION',
+    'sortable' => false,
+    'width' => '10%',
+    'default' => true,
+  ),
+  'ASSIGNED_USER_NAME' => 
+  array (
+    'width' => '9%',
+    'label' => 'LBL_ASSIGNED_TO_NAME',
+    'module' => 'Employees',
+    'id' => 'ASSIGNED_USER_ID',
+    'default' => true,
+  ),
+  'NAME' => 
+  array (
+    'width' => '32%',
+    'label' => 'LBL_NAME',
+    'default' => false,
+    'link' => true,
+  ),
+  'TEAM_NAME' => 
+  array (
+    'width' => '9%',
+    'label' => 'LBL_TEAM',
+    'default' => false,
+  ),
+);
+?>
