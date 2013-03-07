311403152233385e54ce373c6118ea3766773e2c
Saeed <saeed@datasyncorp.com>
2013-03-07 20:18:51 +0500

    ProTracker-Create PDF of Chart Fix

--- custom/modules/Accounts/printNetWorthGraph.php
+++ custom/modules/Accounts/printNetWorthGraph.php
@@ -1,15 +1,23 @@
 <?php
 require_once('custom/modules/Accounts/lib/DataHelper.php');
+require_once('include/tcpdf/tcpdf.php');
 global $db;
 $data4graph = DataHelper::getData($_REQUEST['accounts_id']);
 $html = DataHelper::getHTML($data4graph);
-$pdfContent = DataHelper::getPdfContent($html);
-//Showing PDF for download
 ob_clean();
-header("Content-type: application/pdf;");
-header("Content-Disposition: attachment; filename=clientNetWorthHistory.pdf");
-header("Content-Length: " . strlen($pdfContent));
+$pdf = new TCPDF('L', PDF_UNIT, 'A4', true, 'UTF-8', false);		 
+		// set font
+$pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
+$pdf->SetFont('times', '', 16);
+ 
+// add a page
+$pdf->AddPage();
 
-echo $pdfContent;
+// print html formated text
+$pdf->writeHtml($html);
+$pdf->Image('cache/images/clientNetWorthHistory.png', 15, 33, 0, 156);
+//Close and output PDF document
+$pdf->Output('clientNetWorthHistory.pdf', 'D');
 exit();
 ?>
