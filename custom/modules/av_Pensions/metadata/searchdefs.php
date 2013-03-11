569f8ac450123eaf15db4accfc84ec0ecb5dd537
Ron Walters <ron.walters@protracker.com>
2013-03-11 10:04:01 -0400

    Pensions module: layout updates

---
+++ custom/modules/av_Pensions/metadata/searchdefs.php
@@ -0,0 +1,94 @@
+<?php
+$module_name = 'av_Pensions';
+$searchdefs [$module_name] = 
+array (
+  'layout' => 
+  array (
+    'basic_search' => 
+    array (
+      'name' => 
+      array (
+        'name' => 'name',
+        'default' => true,
+        'width' => '10%',
+      ),
+      'accounts_name' => 
+      array (
+        'type' => 'relate',
+        'link' => true,
+        'label' => 'LBL_ACCOUNTS_NAME',
+        'id' => 'ACCOUNT_ID',
+        'width' => '10%',
+        'default' => true,
+        'name' => 'accounts_name',
+      ),
+      'contacts_name' => 
+      array (
+        'type' => 'relate',
+        'link' => true,
+        'label' => 'LBL_CONTACTS_NAME',
+        'id' => 'CONTACT_ID',
+        'width' => '10%',
+        'default' => true,
+        'name' => 'contacts_name',
+      ),
+    ),
+    'advanced_search' => 
+    array (
+      'name' => 
+      array (
+        'name' => 'name',
+        'default' => true,
+        'width' => '10%',
+      ),
+      'accounts_name' => 
+      array (
+        'type' => 'relate',
+        'link' => true,
+        'label' => 'LBL_ACCOUNTS_NAME',
+        'width' => '10%',
+        'default' => true,
+        'id' => 'ACCOUNT_ID',
+        'name' => 'accounts_name',
+      ),
+      'contacts_name' => 
+      array (
+        'type' => 'relate',
+        'link' => true,
+        'label' => 'LBL_CONTACTS_NAME',
+        'width' => '10%',
+        'default' => true,
+        'id' => 'CONTACT_ID',
+        'name' => 'contacts_name',
+      ),
+      'description' => 
+      array (
+        'type' => 'text',
+        'label' => 'LBL_DESCRIPTION',
+        'sortable' => false,
+        'width' => '10%',
+        'default' => true,
+        'name' => 'description',
+      ),
+      'benefit_start_date' => 
+      array (
+        'type' => 'date',
+        'default' => true,
+        'label' => 'LBL_BENEFIT_START_DATE',
+        'width' => '10%',
+        'name' => 'benefit_start_date',
+      ),
+    ),
+  ),
+  'templateMeta' => 
+  array (
+    'maxColumns' => '3',
+    'maxColumnsBasic' => '4',
+    'widths' => 
+    array (
+      'label' => '10',
+      'field' => '30',
+    ),
+  ),
+);
+?>
