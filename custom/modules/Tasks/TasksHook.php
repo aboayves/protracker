effd2753a8e2489d8a92a0e61b3df60d6d17e55d
Saeed <saeed@datasyncorp.com>
2013-03-12 12:32:45 +0500

    PROTRACK- TasksHook fix

--- custom/modules/Tasks/TasksHook.php
+++ custom/modules/Tasks/TasksHook.php
@@ -35,7 +35,7 @@ class TasksHook
 	function processRecord(&$bean, $event, $arguments)
 	{
 		global $timedate;
-        if( $bean->status == 'Completed' || $bean->status == 'Deferred')
+        if(!empty($bean->date_due) && ($bean->status == 'Completed' || $bean->status == 'Deferred'))
 		{
 			$bean->date_due = "<span style='color:#000000'>".$timedate->getDatePart($bean->date_due)."</span>";      
 		}
