cb5493620c124ef797eb3ee786be178288c5c5b7
Ron Walters <ron.walters@protracker.com>
2013-03-21 23:40:46 -0400

    Retirement Projections module: layout updates

--- custom/modules/av_Retirement_Projections/metadata/detailviewdefs.php
+++ custom/modules/av_Retirement_Projections/metadata/detailviewdefs.php
@@ -30,6 +30,8 @@ array (
           'field' => '30',
         ),
       ),
+      'useTabs' => false,
+      'syncDetailEditViews' => true,
     ),
     'panels' => 
     array (
@@ -45,21 +47,6 @@ array (
           0 => 'team_name',
         ),
         2 => 
-        array (
-          0 => 
-          array (
-            'name' => 'date_entered',
-            'customCode' => '{$fields.date_entered.value} {$APP.LBL_BY} {$fields.created_by_name.value}',
-            'label' => 'LBL_DATE_ENTERED',
-          ),
-          1 => 
-          array (
-            'name' => 'date_modified',
-            'customCode' => '{$fields.date_modified.value} {$APP.LBL_BY} {$fields.modified_by_name.value}',
-            'label' => 'LBL_DATE_MODIFIED',
-          ),
-        ),
-        3 => 
         array (
           0 => 'description',
         ),
