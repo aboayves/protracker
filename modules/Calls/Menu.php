56d8a89e57ea199b58e26f2cd1712510f8d01b5b
Ron Walters <ron.walters@protracker.com>
2013-03-22 19:45:05 -0400

    Activity Types module: added context menu options

--- modules/Calls/Menu.php
+++ modules/Calls/Menu.php
@@ -40,4 +40,6 @@ global $mod_strings,$app_strings;
 if(ACLController::checkAccess('Calls', 'edit', true))$module_menu[]=Array("index.php?module=Calls&action=EditView&return_module=Calls&return_action=DetailView", $mod_strings['LNK_NEW_CALL'],"CreateCalls");
 if(ACLController::checkAccess('Calls', 'list', true))$module_menu[]=Array("index.php?module=Calls&action=index&return_module=Calls&return_action=DetailView", $mod_strings['LNK_CALL_LIST'],"Calls");
 if(ACLController::checkAccess('Calls', 'import', true))$module_menu[] =Array("index.php?module=Import&action=Step1&import_module=Calls&return_module=Calls&return_action=index", $mod_strings['LNK_IMPORT_CALLS'],"Import", 'Calls');
+if(ACLController::checkAccess('av_Activity_Types', 'view', true))$module_menu[] =Array("index.php?module=av_Activity_Types&action=index", "View Activity Types","av_Activity_Types", 'av_Activity_Types');
+
 ?>
