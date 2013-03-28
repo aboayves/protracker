d4788f4a44c878b6e1d4c1d9ac3a7df5ae0d13bb
Ron Walters <ron.walters@protracker.com>
2013-03-27 20:50:49 -0400

    Beneficiaries module: layout updates 3

---
+++ custom/Extension/modules/av_Beneficiaries/Ext/Vardefs/sugarfield_name.php
@@ -0,0 +1,7 @@
+<?php
+ // created: 2013-03-27 19:19:10
+$dictionary['av_Beneficiaries']['fields']['name']['required']=false;
+$dictionary['av_Beneficiaries']['fields']['name']['unified_search']=false;
+$dictionary['av_Beneficiaries']['fields']['name']['calculated']=false;
+
+ ?>
