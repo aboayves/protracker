a3aee3cc847e8cbaf6a2f1809dd67f27da645802
Ron Walters <ron.walters@protracker.com>
2013-04-07 14:15:27 -0400

    Relatives module: layout updates

--- custom/modules/av_Relatives/language/en_us.lang.php
+++ custom/modules/av_Relatives/language/en_us.lang.php
@@ -1,11 +1,11 @@
 <?php
-// created: 2013-02-23 03:12:08
+// created: 2013-04-07 18:05:15
 $mod_strings = array (
-  'LNK_NEW_RECORD' => 'Create Family',
-  'LNK_LIST' => 'View Family',
-  'LNK_IMPORT_AV_RELATIVES' => 'Import Family',
-  'LBL_LIST_FORM_TITLE' => 'Family List',
-  'LBL_SEARCH_FORM_TITLE' => 'Search Family',
-  'LBL_HOMEPAGE_TITLE' => 'My Family',
+  'LNK_NEW_RECORD' => 'Create Relatives',
+  'LNK_LIST' => 'View Relatives',
+  'LNK_IMPORT_AV_RELATIVES' => 'Import Relatives',
+  'LBL_LIST_FORM_TITLE' => 'Relative List',
+  'LBL_SEARCH_FORM_TITLE' => 'Search Relative',
+  'LBL_HOMEPAGE_TITLE' => 'My Relatives',
   'LBL_NAME' => 'Name',
 );
