2b75ac1fb11cebb059bb1e070ba7e77d326828fc
Ron Walters <ron.walters@protracker.com>
2013-04-07 14:12:33 -0400

    Leads module: layout updates

---
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_profession.php
@@ -0,0 +1,20 @@
+<?php
+// created: 2013-04-04 21:12:41
+$dictionary["Lead"]["fields"]["profession"] = array (
+	'name' => 'profession',
+	'vname' => 'LBL_PROFESSION',
+	'type' => 'varchar',
+	'len' => '255',
+	'audited' => 0,
+	'required' => 0,
+	'comment' => '',
+	'default' => '',
+	'massupdate' => True,
+	'reportable' => True,
+	'importable' => True,
+	'duplicate_merge' => 'Enabled',
+	'duplicate_merge_dom_value' => True,
+	'help' => '',
+	'group' => 'Employment',
+);
+?>
