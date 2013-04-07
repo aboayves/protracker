b4db796dd7fb198828315223192fa9c1fb2313d4
Ron Walters <ron.walters@protracker.com>
2013-04-07 14:13:05 -0400

    Companies module: layout updates

---
+++ custom/Extension/modules/av_Companies/Ext/Vardefs/sugarfield_employer_id_Contact.php
@@ -0,0 +1,11 @@
+<?php
+// created: 2013-04-04 21:12:53
+$dictionary["av_Companies"]["fields"]["av_companies_employer_id"] = array (
+	'name' => 'av_companies_employer_id',
+	'type' => 'link',
+	'relationship' => 'employer_id_av_companies',
+	'source' => 'non-db',
+	'side' => 'right',
+	'vname' => 'LBL_CONTACT',
+);
+?>
