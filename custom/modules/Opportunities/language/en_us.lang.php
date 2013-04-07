27b7f3ca5168d6a77943e05c60b2dd7acd0495bf
Ron Walters <ron.walters@protracker.com>
2013-04-07 14:14:48 -0400

    Opportunities module: layout updates

--- custom/modules/Opportunities/language/en_us.lang.php
+++ custom/modules/Opportunities/language/en_us.lang.php
@@ -1,8 +1,9 @@
 <?php
-// created: 2013-03-29 00:54:37
+// created: 2013-04-05 20:21:42
 $mod_strings = array (
   'LBL_ACCOUNT_NAME' => 'Client Name:',
   'LBL_LIST_ACCOUNT_NAME' => 'Client',
   'LBL_LIST_OPPORTUNITY_NAME' => 'Opportunity',
   'LBL_AMOUNT' => 'Opportunity Amount:',
+  'LBL_TAGS_OPPORTUNITIES_FROM_TAGS_TITLE' => 'Tags',
 );
