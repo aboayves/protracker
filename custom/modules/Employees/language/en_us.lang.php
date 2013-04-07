260d105b1127d35a32788539a0d420e5351dd1fe
Ron Walters <ron.walters@protracker.com>
2013-04-07 14:14:24 -0400

    Employees module: layout updates

--- custom/modules/Employees/language/en_us.lang.php
+++ custom/modules/Employees/language/en_us.lang.php
@@ -1,5 +1,11 @@
 <?php
-// created: 2012-05-17 13:35:17
+// created: 2013-04-04 20:29:17
 $mod_strings = array (
   'LBL_RELATIONSHIP_LISTING' => 'Relationship Type',
+  'LBL_EDITVIEW_PANEL1' => 'Address & Phone',
+  'LBL_EDITVIEW_PANEL2' => 'Permissions',
+  'LBL_SUGAR_LOGIN' => 'Is ProTracker User',
+  'LBL_PRODUCT_LEVEL' => 'Product Edition',
+  'LBL_IS_MOBILE_USER' => 'Is Mobile User',
+  'LBL_DEPARTMENT' => 'Position',
 );
