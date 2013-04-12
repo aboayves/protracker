4f5b75b8fa953330cec07ecffe29881c4f5a0ae4
Ron Walters <ron.walters@protracker.com>
2013-04-12 13:46:31 -0400

    Life Insurance Beneficiaries module: vardef update

--- custom/Extension/modules/av_Life_Insurance/Ext/Vardefs/sugarfield_life_insurance_id_av_Life_Insurance_Beneficiaries.php
+++ custom/Extension/modules/av_Life_Insurance/Ext/Vardefs/sugarfield_life_insurance_id_av_Life_Insurance_Beneficiaries.php
@@ -1,11 +1,13 @@
 <?php
 // created: 2013-04-10 15:30:45
-$dictionary["av_Life_Insurance"]["fields"]["av_life_insurance_life_insurance_id"] = array (
-	'name' => 'av_life_insurance_life_insurance_id',
+$dictionary["av_Life_Insurance"]["fields"]["av_life_insurance_beneficiaries_av_life_insurance"] = array (
+	'name' => 'av_life_insurance_beneficiaries_av_life_insurance', 
 	'type' => 'link',
 	'relationship' => 'life_insurance_id_av_life_insurance',
 	'source' => 'non-db',
 	'side' => 'right',
 	'vname' => 'LBL_AV_LIFE_INSURANCE_BENEFICIARIES',
 );
+
+//av_life_insurance_life_insurance_id',
 ?>
