067de83beffba84e4d802dcb7be09c5859b91e17
Ron Walters <ron.walters@protracker.com>
2013-04-12 13:46:11 -0400

    Employees module: listview update

---
+++ custom/modules/Employees/metadata/listviewdefs.php
@@ -0,0 +1,110 @@
+<?php
+$listViewDefs ['Employees'] = 
+array (
+  'IS_ADMIN' => 
+  array (
+    'type' => 'bool',
+    'default' => false,
+    'studio' => 
+    array (
+      'listview' => false,
+      'searchview' => false,
+      'related' => false,
+    ),
+    'label' => 'LBL_IS_ADMIN',
+    'width' => '10%',
+  ),
+  'NAME' => 
+  array (
+    'width' => '20%',
+    'label' => 'LBL_LIST_NAME',
+    'link' => true,
+    'related_fields' => 
+    array (
+      0 => 'last_name',
+      1 => 'first_name',
+    ),
+    'orderBy' => 'last_name',
+    'default' => true,
+  ),
+  'DEPARTMENT' => 
+  array (
+    'width' => '10%',
+    'label' => 'LBL_DEPARTMENT',
+    'link' => true,
+    'default' => true,
+  ),
+  'REPORTS_TO_NAME' => 
+  array (
+    'width' => '15%',
+    'label' => 'LBL_LIST_REPORTS_TO_NAME',
+    'link' => true,
+    'sortable' => false,
+    'default' => true,
+  ),
+  'EMAIL1' => 
+  array (
+    'width' => '15%',
+    'label' => 'LBL_LIST_EMAIL',
+    'link' => true,
+    'customCode' => '{$EMAIL1_LINK}{$EMAIL1}</a>',
+    'default' => true,
+    'sortable' => false,
+  ),
+  'PHONE_WORK' => 
+  array (
+    'width' => '10%',
+    'label' => 'LBL_LIST_PHONE',
+    'link' => true,
+    'default' => true,
+  ),
+  'PRODUCT_LEVEL' => 
+  array (
+    'type' => 'varchar',
+    'default' => true,
+    'label' => 'LBL_PRODUCT_LEVEL',
+    'width' => '15%',
+  ),
+  'TITLE' => 
+  array (
+    'width' => '15%',
+    'label' => 'LBL_TITLE',
+    'link' => true,
+    'default' => false,
+  ),
+  'DESCRIPTION' => 
+  array (
+    'type' => 'text',
+    'label' => 'LBL_DESCRIPTION',
+    'sortable' => false,
+    'width' => '10%',
+    'default' => false,
+  ),
+  'DATE_MODIFIED' => 
+  array (
+    'type' => 'datetime',
+    'studio' => 
+    array (
+      'editview' => false,
+      'quickcreate' => false,
+      'wirelesseditview' => false,
+    ),
+    'label' => 'LBL_DATE_MODIFIED',
+    'width' => '10%',
+    'default' => false,
+  ),
+  'DATE_ENTERED' => 
+  array (
+    'width' => '10%',
+    'label' => 'LBL_DATE_ENTERED',
+    'default' => false,
+  ),
+  'EMPLOYEE_STATUS' => 
+  array (
+    'width' => '10%',
+    'label' => 'LBL_LIST_EMPLOYEE_STATUS',
+    'link' => false,
+    'default' => false,
+  ),
+);
+?>
