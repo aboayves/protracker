7fe5be8a9a78f600ccae2d56a884a245fa923be1
Ron Walters <ron.walters@protracker.com>
2013-04-15 11:00:38 -0400

    Tax Returns module: label updates

--- custom/modules/av_Tax_Returns/language/en_us.lang.php
+++ custom/modules/av_Tax_Returns/language/en_us.lang.php
@@ -1,9 +1,10 @@
 <?php
-// created: 2013-04-02 20:48:25
+// created: 2013-04-12 20:54:23
 $mod_strings = array (
   'LBL_EDITVIEW_PANEL1' => 'Income and Liability',
   'LBL_NAME' => 'Name',
   'LBL_EDITVIEW_PANEL2' => 'Capital',
   'LBL_EDITVIEW_PANEL3' => 'Filing',
   'LBL_QUICKCREATE_PANEL1' => 'New Panel 1',
+  'LBL_TAX_YEAR' => 'Tax Year',
 );
