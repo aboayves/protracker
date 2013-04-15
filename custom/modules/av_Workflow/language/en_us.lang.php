e325a223338848ee7c63844d2cbde9bed5717f3d
Ron Walters <ron.walters@protracker.com>
2013-04-15 11:00:17 -0400

    Workflow module: pluralized workflow and task template modules

--- custom/modules/av_Workflow/language/en_us.lang.php
+++ custom/modules/av_Workflow/language/en_us.lang.php
@@ -1,6 +1,13 @@
 <?php
-// created: 2012-11-01 07:51:49
+// created: 2013-04-15 02:59:51
 $mod_strings = array (
   'LBL_DETAILVIEW_PANEL1' => 'New Panel 1',
   'LBL_ASSIGN_TO_MEMBERS' => 'Assign workflow to every group member',
+  'LNK_NEW_RECORD' => 'Create Workflows',
+  'LNK_LIST' => 'View Workflowss',
+  'LBL_LIST_FORM_TITLE' => 'Workflow List',
+  'LBL_SEARCH_FORM_TITLE' => 'Search Workflow',
+  'LBL_MODULE_NAME' => 'Workflows',
+  'LBL_RT_WORKFLOW_RT_TASK_TEMPLATE' => 'Task Templatess',
+  'LBL_HOMEPAGE_TITLE' => 'My Workflows',
 );
