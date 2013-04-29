3d0c17058e4f566ba98ade6845114b6c083787db
Saeed <saeed@datasyncorp.com>
2013-04-29 11:57:16 +0500

    ProTrack- Update Account History bad labels fixed in Admin area

---
+++ custom/Extension/modules/Administration/Ext/Language/en_us.update_account_histories.php
@@ -0,0 +1,3 @@
+<?php
+$mod_strings['LBL_UPDATE_ACCOUNT_HISTORY'] = 'Record Account Status For All Clients';
+$mod_strings['LBL_REG_UPDATE_ACCOUNT_HISTORY'] = 'Record Account History For Each Client\'s Account';
