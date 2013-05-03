81089f6f6ffc98162cb3f1e87708e602ddd63219
Ron Walters <ron.walters@protracker.com>
2013-05-03 16:18:38 -0400

    Clients module: added privacy notice, adv offer dates

---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_last_privacy_notice.php
@@ -0,0 +1,19 @@
+<?php
+// created: 2013-05-03 16:12:54
+$dictionary["Account"]["fields"]["last_privacy_notice"] = array (
+	'name' => 'last_privacy_notice',
+	'vname' => 'LBL_LAST_PRIVACY_NOTICE',
+	'type' => 'date',
+	'audited' => 0,
+	'required' => 0,
+	'comment' => '',
+	'default' => '',
+	'massupdate' => True,
+	'reportable' => True,
+	'importable' => True,
+	'duplicate_merge' => 'Enabled',
+	'duplicate_merge_dom_value' => True,
+	'help' => '',
+	'group' => '',
+);
+?>
