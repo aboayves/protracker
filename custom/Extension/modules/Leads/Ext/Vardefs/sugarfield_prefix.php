a2e8f5a533cf3b91dfba480384bf78cc45976d4d
Ron Walters <ron.walters@protracker.com>
2013-05-09 13:45:21 -0400

    Leads module: added prefix, suffix name fields

---
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_prefix.php
@@ -0,0 +1,20 @@
+<?php
+// created: 2013-05-09 13:25:45
+$dictionary["Lead"]["fields"]["prefix"] = array (
+	'name' => 'prefix',
+	'vname' => 'LBL_PREFIX',
+	'type' => 'varchar',
+	'len' => '20',
+	'audited' => 0,
+	'required' => 0,
+	'comment' => '',
+	'default' => '',
+	'massupdate' => True,
+	'reportable' => True,
+	'importable' => True,
+	'duplicate_merge' => 'Enabled',
+	'duplicate_merge_dom_value' => True,
+	'help' => '',
+	'group' => 'Name',
+);
+?>
---
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_suffix.php
@@ -0,0 +1,20 @@
+<?php
+// created: 2012-12-17 16:58:58
+$dictionary["Lead"]["fields"]["suffix"] = array (
+	'name' => 'suffix',
+	'vname' => 'LBL_SUFFIX',
+	'type' => 'varchar',
+	'len' => '20',
+	'audited' => 0,
+	'required' => 0,
+	'comment' => '',
+	'default' => '',
+	'massupdate' => True,
+	'reportable' => True,
+	'importable' => True,
+	'duplicate_merge' => 'Enabled',
+	'duplicate_merge_dom_value' => True,
+	'help' => '',
+	'group' => 'Name',
+);
+?>
