461315e91310fb48a1423478aaa1501aaee8e237
Uneebe <uneebe@datasyncorp.com>
2013-05-17 02:05:29 +0500

    PROTRACK-529

--- custom/modules/Accounts/logic_hooks.php
+++ custom/modules/Accounts/logic_hooks.php
@@ -7,6 +7,7 @@ $hook_array['after_save'] = Array();
 $hook_array['after_save'][] = Array(1, ' sync email address', 'custom/modules/Accounts/AccountsHook.php', 'AccountsHook', 'sync_email_address');
 $hook_array['after_relationship_add'] = Array();
 $hook_array['after_relationship_add'][] = Array(1, 'set first contact to primary', 'custom/modules/Accounts/AccountsHook.php', 'AccountsHook', 'setFirstContactToPrimary');
+$hook_array['after_relationship_add'][] = Array(2, 'clear old relationships', 'custom/modules/Accounts/AccountsHook.php', 'AccountsHook', 'clearOldRelationships');
 $hook_array['before_delete'] = Array();
 $hook_array['before_delete'][] = Array(1, 'delete child records', 'custom/modules/Accounts/AccountsHook.php', 'AccountsHook', 'delete_child_records');
 
