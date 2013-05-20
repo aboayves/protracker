07b6d09956edae1d3ebd05f4252ca1e8bdabd613
Saeed <saeed@datasyncorp.com>
2013-05-20 15:57:43 +0500

    PROTRACK-590 Fixed "Undefined Message"

---
+++ custom/Extension/application/Ext/Language/en_us.remove_attachments.php
@@ -0,0 +1,2 @@
+<?php
+$app_strings['LBL_REMOVING_ATTACHMENT'] = 'Removing attachment...';
--- jssource/src_files/include/SugarFields/Fields/File/SugarFieldFile.js
+++ jssource/src_files/include/SugarFields/Fields/File/SugarFieldFile.js
@@ -19,7 +19,7 @@ if ( typeof(SUGAR.field) == 'undefined' ) {
 if ( typeof(SUGAR.field.file) == 'undefined' ) {
     SUGAR.field.file = {
         deleteAttachment: function(elemBaseName,docTypeName,elem) {
-            ajaxStatus.showStatus("Removing attachment...");
+            ajaxStatus.showStatus(SUGAR.language.get("app_strings", "LBL_REMOVING_ATTACHMENT"));
             elem.form.deleteAttachment.value=1;
             elem.form.action.value="deleteattachment";
 
