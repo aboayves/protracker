e36525fbd9ea88ea33144a54c9177e18b039e181
Uneebe <uneebe@datasyncorp.com>
2013-05-22 16:20:59 +0500

    PROTRACK-618

--- custom/Extension/application/Ext/Language/en_us.delete_to_suppress.php
+++ custom/Extension/application/Ext/Language/en_us.delete_to_suppress.php
@@ -1,3 +1,5 @@
 <?php
 $app_strings['LBL_DELETE_BUTTON_LABEL'] = 'Suppress';
 $app_strings['LBL_DELETE_BUTTON_TITLE'] = 'Suppress';
+$app_strings['NTC_DELETE_CONFIRMATION_NUM'] = 'Are you sure you want to suppress the ';
+$app_strings['NTC_DELETE_CONFIRMATION'] = 'Are you sure you want to suppress this record?';
