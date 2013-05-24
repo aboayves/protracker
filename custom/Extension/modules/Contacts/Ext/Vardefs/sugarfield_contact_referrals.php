d5b30f728c7c75cbf850c998ac7e90fccad9ab1f
Saeed <saeed@datasyncorp.com>
2013-05-24 15:51:44 +0500

    PROTRACK-619

--- custom/Extension/modules/Contacts/Ext/Vardefs/sugarfield_contact_referrals.php
+++ custom/Extension/modules/Contacts/Ext/Vardefs/sugarfield_contact_referrals.php
@@ -1,11 +1,12 @@
 <?php
 // created: 2013-02-20 22:57:40
-$dictionary["Contact"]["fields"]["contact_referrals"] = array (
+//This link field is not using any where and delete functionality is suffering due to this, So commented.
+/*$dictionary["Contact"]["fields"]["contact_referrals"] = array (
 	'name' => 'contact_referrals',
 	'type' => 'link',
 	'relationship' => 'contact_referrals',
 	'source' => 'non-db',
 	'side' => 'right',
 	'vname' => 'LBL_CONTACT',
-);
+);*/
 ?>
