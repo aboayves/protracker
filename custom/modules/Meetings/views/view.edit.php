34a475dea6c7f02b2bb748a24fb5891480a28dca
Saeed <saeed@datasyncorp.com>
2013-05-29 16:38:25 +0500

    PROTRACK-621

---
+++ custom/modules/Meetings/views/view.edit.php
@@ -0,0 +1,40 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+
+/*********************************************************************************
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement (“MSA”), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright (C) 2004-2013 SugarCRM Inc.  All rights reserved.
+ ********************************************************************************/
+
+
+require_once('modules/Meetings/views/view.edit.php');
+
+class CustomMeetingsViewEdit extends MeetingsViewEdit
+{
+ 	/**
+ 	 * @see SugarView::display()
+ 	 */
+ 	public function display()
+ 	{ 
+ 		parent::display();
+		echo "<style>
+				#search_first_name{
+				  width: 70%;
+				}
+				#search_last_name{
+				  width: 70%;
+				}
+				#search_email{
+				  width: 70%;
+				}
+			  </style>";
+ 	}
+}
