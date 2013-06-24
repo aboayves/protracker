9331b210e2e0942001aa235969c701159cb34646
Saeed <saeed@datasyncorp.com>
2013-06-24 17:11:00 +0500

    Accounts:Listview populated missing fields

--- custom/modules/av_Account_Histories/logic_hooks.php
+++
@@ -1,4 +0,0 @@
-<?php
-$hook_array['process_record'] = Array();
-$hook_array['process_record'][] = Array(1, 'fill accounts list view', 'custom/modules/av_Account_Histories/av_Account_HistoriesHook.php', 'av_Account_HistoriesHook', 'fill_account_listview');
-?>
