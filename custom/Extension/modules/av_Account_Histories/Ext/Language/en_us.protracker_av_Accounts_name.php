db509ffc103181fe362b061c20625ac17731f443
Saeed <saeed@datasyncorp.com>
2013-07-03 17:19:40 +0500

    PROTRACK-684

--- custom/Extension/modules/av_Account_Histories/Ext/Language/en_us.protracker_av_Accounts_name.php
+++ custom/Extension/modules/av_Account_Histories/Ext/Language/en_us.protracker_av_Accounts_name.php
@@ -1,3 +1,5 @@
 <?php
 $mod_strings['LBL_AV_ACCOUNTS_NAME']='Account';
+$mod_strings['LBL_ACCOUNTS_AV_ACCOUNTS_FROM_ACCOUNTS_TITLE']='Account';
+$mod_strings['LBL_AV_ACCOUNTS_ID']='Account ID';
 ?>
--- custom/Extension/modules/av_Account_Histories/Ext/Vardefs/sugarfield_av_accounts_history.php
+++ custom/Extension/modules/av_Account_Histories/Ext/Vardefs/sugarfield_av_accounts_history.php
@@ -5,6 +5,7 @@ $dictionary["av_Account_Histories"]["fields"]["av_accounts_id"] = array (
   'type' => 'id',
   'relationship' => 'av_account_histories_av_accounts',
   'module'=>'av_Account_Histories',
+  'vname' => 'LBL_AV_ACCOUNTS_ID',
   'duplicate_merge' => 'disabled',
   'len' => 36,
   'size' => '20',
