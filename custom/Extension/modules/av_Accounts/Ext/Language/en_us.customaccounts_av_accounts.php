87dc20f03095c803a256dd636d936c7738614a26
Saeed <saeed@datasyncorp.com>
2013-07-05 15:51:53 +0500

    PROTRACK-685

--- custom/Extension/modules/av_Accounts/Ext/Language/en_us.customaccounts_av_accounts.php
+++ custom/Extension/modules/av_Accounts/Ext/Language/en_us.customaccounts_av_accounts.php
@@ -1,2 +1,4 @@
 <?php
 $mod_strings['LBL_ACCOUNTS_AV_ACCOUNTS_FROM_ACCOUNTS_TITLE'] = 'Clients';
+$mod_strings['LBL_ACCOUNT_ID'] = 'Client ID';
+$mod_strings['LBL_ACCOUNT_NAME'] = 'Client Name';
--- custom/Extension/modules/av_Accounts/Ext/Language/en_us.protracker_account_type_id.php
+++ custom/Extension/modules/av_Accounts/Ext/Language/en_us.protracker_account_type_id.php
@@ -1,3 +1,4 @@
 <?php
-$mod_strings['LBL_ACCOUNT_TYPE_ID']='Account Type';
+$mod_strings['LBL_ACCOUNT_TYPE_ID']='Account Type ID';
+$mod_strings['LBL_ACCOUNT_TYPE']='Account Type';
 ?>
