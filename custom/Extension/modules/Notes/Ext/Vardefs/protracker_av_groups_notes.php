5c49130015fed02c5aa8befe4bd383ed33abbef8
Saeed <saeed@datasyncorp.com>
2013-07-08 14:35:22 +0500

    PROTRACKER-697

--- custom/Extension/modules/Notes/Ext/Vardefs/protracker_av_groups_notes.php
+++ custom/Extension/modules/Notes/Ext/Vardefs/protracker_av_groups_notes.php
@@ -2,6 +2,7 @@
 // created: 2012-05-21 17:10:30
 $dictionary["Note"]["fields"]["av_group_id"] = array (
   'name' => 'av_group_id',
+  'vname' => 'LBL_AV_GROUPS_ID',
   'type' => 'id',
   'relationship' => 'av_groups_notes',
   'len' => 36,
