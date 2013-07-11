7f32ecc0680c917b40b67eb02d67aae90b2428d5
Saeed <saeed@datasyncorp.com>
2013-07-11 09:53:09 +0500

    PROTRACK-700

--- custom/Extension/modules/Tags/Ext/Vardefs/SugarTags_Tags.php
+++ custom/Extension/modules/Tags/Ext/Vardefs/SugarTags_Tags.php
@@ -3,6 +3,7 @@ $dictionary["Tags"]["fields"]["tags_accounts"] = array (
   'name' => 'tags_accounts',
   'type' => 'link',
   'relationship' => 'tags_accounts',
+  'vname' => 'LBL_TAGS_ACCOUNTS_FROM_CLIENTS_TITLE',
   'source' => 'non-db',
 );
 
@@ -10,6 +11,7 @@ $dictionary["Tags"]["fields"]["tags_contacts"] = array (
   'name' => 'tags_contacts',
   'type' => 'link',
   'relationship' => 'tags_contacts',
+  'vname' => 'LBL_TAGS_CONTACTS_FROM_CONTACTS_TITLE',
   'source' => 'non-db',
 );
 
@@ -17,6 +19,7 @@ $dictionary["Tags"]["fields"]["tags_leads"] = array (
   'name' => 'tags_leads',
   'type' => 'link',
   'relationship' => 'tags_leads',
+  'vname' => 'LBL_TAGS_LEADS_FROM_LEADS_TITLE',
   'source' => 'non-db',
 );
 
@@ -24,6 +27,7 @@ $dictionary["Tags"]["fields"]["tags_opportunities"] = array (
   'name' => 'tags_opportunities',
   'type' => 'link',
   'relationship' => 'tags_opportunities',
+  'vname' => 'LBL_TAGS_OPPORTUNITIES_FROM_OPPORTUNITIES_TITLE',
   'source' => 'non-db',
 );
 ?>
