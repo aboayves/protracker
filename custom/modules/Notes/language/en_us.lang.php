38913787c8d1b17cc9a12ed21c57b41ad0ed249d
Ron Walters <ron.walters@protracker.com>
2013-07-11 15:43:18 -0400

    FB-38021

--- custom/modules/Notes/language/en_us.lang.php
+++ custom/modules/Notes/language/en_us.lang.php
@@ -1,11 +1,11 @@
 <?php
-// created: 2013-03-29 17:24:25
+// created: 2013-07-11 19:35:36
 $mod_strings = array (
   'LBL_DESCRIPTION' => 'Note',
   'LBL_AV_ACTIVITY_TYPES_NOTES_1_FROM_AV_ACTIVITY_TYPES_TITLE' => 'Activity Type',
   'LBL_CREATED_BY' => 'Created By',
   'LBL_EDITVIEW_PANEL1' => 'Settings',
-  'LBL_AV_GROUPS_TITLE' => 'Group Title',
+  'LBL_AV_GROUPS_TITLE' => 'Group Name',
   'LBL_AV_GROUPS_ID' => 'Group ID',
   'LBL_NAME' => 'Subject',
 );
