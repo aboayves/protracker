e210d0e72493ef4d38bda38e05788fcfe6d44a39
Saeed <saeed@datasyncorp.com>
2013-07-15 13:58:06 +0500

    PROTRACK-726

---
+++ custom/modules/av_Workflow/metadata/detailviewdefs.php
@@ -0,0 +1,85 @@
+<?php
+$module_name = 'av_Workflow';
+$viewdefs [$module_name] = 
+array (
+  'DetailView' => 
+  array (
+    'templateMeta' => 
+    array (
+      'form' => 
+      array (
+        'buttons' => 
+        array (
+          0 => 'EDIT',
+          1 => 'DUPLICATE',
+          2 => 'DELETE',
+          3 => 'FIND_DUPLICATES',
+		  4 => 
+		  array(
+		  	'customCode' => '<input title="Assign Workflow" type="button" name="assignWorkflow" id="assignWorkflow" onclick="setCheckedTemplateIDs(); var _form = document.getElementById(\'formDetailView\');_form.action.value=\'assign\';SUGAR.ajaxUI.submitForm(_form);" value="Assign Workflow">'
+		  ),
+        ),
+      ),
+      'maxColumns' => '2',
+      'widths' => 
+      array (
+        0 => 
+        array (
+          'label' => '10',
+          'field' => '30',
+        ),
+        1 => 
+        array (
+          'label' => '10',
+          'field' => '30',
+        ),
+      ),
+      'useTabs' => false,
+      'syncDetailEditViews' => true,
+	  'includes' => 
+      array (
+        0 => 
+        array (
+          'file' => 'modules/av_Workflow/tree.js',
+        ),
+		1 => 
+        array (
+          'file' => 'modules/av_Workflow/delete.js',
+        ),
+      ),
+    ),
+    'panels' => 
+    array (
+      'default' => 
+      array (
+        0 => 
+        array (
+          0 => 'name',
+          1 => 'assigned_user_name',
+        ),
+        1 => 
+        array (
+          0 => 'team_name',
+          1 => 'av_activity_types_name',
+        ),
+        2 => 
+        array (
+          0 => 'description',
+        ),
+      ),
+	  'lbl_tree' =>
+	  array(
+		0 =>
+		array(
+			0 =>
+			array(
+				'name' => 'tree',
+				'hideLabel' => true,
+				'customCode' => '<div id="tree_plotting_div" class="whitebg ygtv-checkbox"></div>'  
+			),
+		),    
+	  ),
+    ),
+  ),
+);
+?>
