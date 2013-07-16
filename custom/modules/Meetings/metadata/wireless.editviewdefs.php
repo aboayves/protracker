21631807b6dd6fd453910d57ba9ec3b2d89ba21a
Saeed <saeed@datasyncorp.com>
2013-07-16 10:06:36 +0500

    PROTRACK-711

--- custom/modules/Meetings/metadata/wireless.editviewdefs.php
+++ custom/modules/Meetings/metadata/wireless.editviewdefs.php
@@ -1,86 +1,43 @@
 <?php
-$viewdefs ['Meetings'] = 
-array (
-  'EditView' => 
-  array (
-    'templateMeta' => 
-    array (
-      'maxColumns' => '1',
-      'widths' => 
-      array (
-        0 => 
-        array (
-          'label' => '10',
-          'field' => '30',
-        ),
-      ),
-      'useTabs' => false,
-    ),
-    'panels' => 
-    array (
-      0 => 
-      array (
-        0 => 
-        array (
-          'name' => 'parent_name',
-          'label' => 'LBL_LIST_RELATED_TO',
-        ),
-      ),
-      1 => 
-      array (
-        0 => 'assigned_user_name',
-      ),
-      2 => 
-      array (
-        0 => 
-        array (
-          'name' => 'name',
-          'displayParams' => 
-          array (
-            'required' => true,
-            'wireless_edit_only' => true,
-          ),
-        ),
-      ),
-      3 => 
-      array (
-        0 => 'date_start',
-      ),
-      4 => 
-      array (
-        0 => 
-        array (
-          'name' => 'date_end',
-          'comment' => 'Date meeting ends',
-          'label' => 'LBL_DATE_END',
-        ),
-      ),
-      5 => 
-      array (
-        0 => 
-        array (
-          'name' => 'av_activity_types_name',
-          'label' => 'LBL_AV_ACTIVITY_TYPES_NAME',
-        ),
-      ),
-      6 => 
-      array (
-        0 => 'status',
-      ),
-      7 => 
-      array (
-        0 => 
-        array (
-          'name' => 'location',
-          'comment' => 'Meeting location',
-          'label' => 'LBL_LOCATION',
-        ),
-      ),
-      8 => 
-      array (
-        0 => 'description',
-      ),
-    ),
-  ),
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+/*********************************************************************************
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement (“MSA”), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright (C) 2004-2013 SugarCRM Inc.  All rights reserved.
+ ********************************************************************************/
+
+/*********************************************************************************
+
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________..
+ ********************************************************************************/
+ 
+$viewdefs['Meetings']['EditView'] = array(
+	'templateMeta' => array(
+                            'maxColumns' => '1', 
+                            'widths' => array(
+								array('label' => '10', 'field' => '30'), 
+                            ),                                  
+                           ),
+    'panels' => array(
+		array(array('name'=>'name', 'displayParams'=>array('required'=>true,'wireless_edit_only'=>true,)),),    
+		array('date_start'),
+		array('date_end'),
+		array('status'),
+	    array('duration_hours'),
+		array('duration_minutes'),
+		array('location'),
+        array('description'),	    
+		array('assigned_user_name'),
+		array('team_name'),
+	),
 );
 ?>
