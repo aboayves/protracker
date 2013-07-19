7c55b66c579e3b01bcaf3a473fb7857a66a5b365
Ron Walters <ron.walters@protracker.com>
2013-07-19 14:20:40 -0400

    FB-38057

--- custom/modules/av_Help/controller.php
+++ custom/modules/av_Help/controller.php
@@ -1,16 +1,17 @@
 <?php class av_HelpController extends SugarController{     
 	function action_help(){ 	
 		global $db;
-		$sql = "SELECT helpid FROM av_help WHERE name='".$_REQUEST['help_module']."' AND view='".$_REQUEST['help_action']."' AND deleted = 0";
+		$sql = "SELECT helpid FROM av_help WHERE name='".strtolower($_REQUEST['help_module'])."' AND view='".strtolower($_REQUEST['help_action'])."' AND deleted = 0";
 		$rs = $db->query($sql);
 		while($row = $db->fetchByAssoc($rs))
 		{
 			$helpid = $row['helpid'];
 		} 
 		if (strlen($helpid)>0) {
-			header('Location: http://protracker.zendesk.com/entries/'.$helpid);
+			header('Location: http://help.protrackercloud.com/entries/'.$helpid);
 		} else {
-			die("Help article not found. <A Href='https://protracker.zendesk.com/categories/20101816'>Click here</A> to continue to the knowledge base");
+			//die("Help article not found. <A Href='https://protracker.zendesk.com/categories/20101816'>Click here</A> to continue to the knowledge base");
+			header('Location: https://help.protrackercloud.com/categories/20101816');
 		}
 	}
 } 
